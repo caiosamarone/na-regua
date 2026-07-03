ace0b7e25a3e3bdd36cabfdc00c4553c0c9ee8bb
caiomendes <caiosamarone10@hotmail.com>
2026-07-03 12:11:18 -0300

    feat - [api] initial configuration

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,13 @@
 import Fastify from "fastify";
-
 import { env } from "./config/env";
+import { prisma } from "./config/prisma";
 
 const app = Fastify({ logger: true });
 
 async function start() {
-  await app.listen({ port: 8080, host: "0.0.0.0" });
+  const teste = await prisma.deployTest.count();
+  console.log(teste, "teste");
+  await app.listen({ port: env.PORT, host: "0.0.0.0" });
 }
 
 start();
