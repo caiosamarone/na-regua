877b6077555da32ff6559aa700d10fa651677388
Caio Mendes <caiosamarone10@hotmail.com>
2026-07-21 19:44:59 -0300

    [api] refactor: remove web-push type from tsconfig

---
+++ api/src/@types/web-push.d.ts
@@ -0,0 +1,17 @@
+declare module "web-push" {
+  interface PushSubscription {
+    endpoint: string;
+    keys: { p256dh: string; auth: string };
+  }
+
+  export function setVapidDetails(
+    subject: string,
+    publicKey: string,
+    privateKey: string,
+  ): void;
+
+  export function sendNotification(
+    subscription: PushSubscription,
+    payload: string,
+  ): Promise<void>;
+}
